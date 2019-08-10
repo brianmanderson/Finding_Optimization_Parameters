2287bfc20d7bfb0ca7a3a5dcd9f7fd65e5524595
Brian Mark Anderson <bmanderson@mdanderson.org>
2019-08-10 15:01:14 -0500

    added min_or_max function and plotting labels

--- Plot_Best_Learning_rates.py
+++ Plot_Best_Learning_rates.py
@@ -2,7 +2,7 @@ import os
 from matplotlib import pyplot as plt
 import numpy as np
 
-def make_plot(path, name='acc'):
+def make_plot(path, name='acc', min_or_max = max):
     files = [i for i in os.listdir(path) if i.find('.txt') != -1]
     lrs = []
     metrics = []
@@ -10,8 +10,12 @@ def make_plot(path, name='acc'):
         lrs.append(float(file.strip('.txt')))
         fid = open(os.path.join(path,file))
         for line in fid:
-            if line.find(name) != 0:
-                metrics.append(float(fid.readline().strip('\n').split(',')[1]))
+            if line.find(name) != -1:
+                start = line.find('[')
+                stop = line.find(']')
+                values = line[start+1:stop].split(',')
+                value = min_or_max([float(i) for i in values])
+                metrics.append(value)
                 break
         fid.close()
     indexes = np.argsort(np.asarray(lrs))
@@ -19,13 +23,13 @@ def make_plot(path, name='acc'):
     lrs = np.asarray(lrs)[indexes]
     plt.figure()
     plt.plot(lrs,metrics)
-    plt.ylabel('Dice')
+    plt.ylabel(name)
     plt.xscale('log')
-    plt.title('Dice vs Learning Rate')
+    plt.title(name + ' vs Learning Rate')
     plt.xlabel('Learning Rate')
     plt.show()
     return None
 
 if __name__ == '__main__':
-    path = r'K:\Morfeus\BMAnderson\CNN\Data\Data_Liver\Data\Keras\v3_freezing\Learning_rate_info'
-    make_plot(path,name='acc')
+    path = r'K:\Morfeus\BMAnderson\CNN\Data\Data_Liver\Liver_Segments\Keras\3D_UNet36_Atrous_Block_no_start_conv_2\learning_rates'
+    make_plot(path,name='val_dice_coef_3D',min_or_max=max)
