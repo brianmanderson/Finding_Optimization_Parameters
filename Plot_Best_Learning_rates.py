bbe08840e92d75bcb577e2c678cddf6a6f7320d3
Brian Mark Anderson <bmanderson@mdanderson.org>
2019-08-16 11:02:18 -0500

    generalized

--- Plot_Best_Learning_rates.py
+++ Plot_Best_Learning_rates.py
@@ -2,7 +2,7 @@ import os
 from matplotlib import pyplot as plt
 import numpy as np
 
-def make_plot(path, name='acc', min_or_max = max):
+def make_plot(path, metric='acc', min_or_max = max):
     files = [i for i in os.listdir(path) if i.find('.txt') != -1]
     lrs = []
     metrics = []
@@ -10,7 +10,7 @@ def make_plot(path, name='acc', min_or_max = max):
         lrs.append(float(file.strip('.txt')))
         fid = open(os.path.join(path,file))
         for line in fid:
-            if line.find(name) != -1:
+            if line.find(metric) != -1:
                 start = line.find('[')
                 stop = line.find(']')
                 values = line[start+1:stop].split(',')
@@ -23,13 +23,12 @@ def make_plot(path, name='acc', min_or_max = max):
     lrs = np.asarray(lrs)[indexes]
     plt.figure()
     plt.plot(lrs,metrics)
-    plt.ylabel(name)
+    plt.ylabel(metric)
     plt.xscale('log')
-    plt.title(name + ' vs Learning Rate')
+    plt.title(metric + ' vs Learning Rate')
     plt.xlabel('Learning Rate')
     plt.show()
     return None
 
 if __name__ == '__main__':
-    path = r'K:\Morfeus\BMAnderson\CNN\Data\Data_Liver\Liver_Segments\Keras\3D_UNet36_Atrous_Block_no_start_conv_2\learning_rates'
-    make_plot(path,name='val_dice_coef_3D',min_or_max=max)
+    xxx = 1
