3b24315f41497a7ce3dbdcbcdfed5882f0cc38bd
Brian Mark Anderson <bmanderson@mdanderson.org>
2019-09-23 16:50:28 -0500

    auto finding batchsize

--- LR_Finder.py
+++ LR_Finder.py
@@ -31,10 +31,13 @@ def save_obj(path, obj): # Save almost anything.. dictionary, list, etc.
 
 class LearningRateFinder(object):
     def __init__(self, model, train_generator, stopFactor=4, beta=0.98,metrics=['accuracy'], optimizer=Adam, lower_lr=1e-8,
-                 high_lr=1e-1,epochs=None,batchsize=1,samplesize=2048, steps_per_epoch=None,
+                 high_lr=1e-1,epochs=None,batchsize=None,samplesize=2048, steps_per_epoch=None,
                  loss = 'categorical_crossentropy', out_path=os.path.join('.','Learning_rates')):
         # store the model, stop factor, and beta value (for computing
         # a smoothed, average loss)
+        if batchsize is None:
+            x,y = train_generator.__getitem__(0)
+            batchsize = x.shape[0]
         optimizer = optimizer(lr=lower_lr) # Doesn't really matter, will be over-written anyway
         self.start_lr = lower_lr
         self.stop_lr = high_lr
@@ -44,12 +47,6 @@ class LearningRateFinder(object):
         self.beta = beta
         self.output_dict = {}
 
-        # initialize our learning rate multiplier, average loss, best
-        # loss found thus far, current batch number, and weights file
-        self.lrMult = 1
-        self.avgLoss = 0
-        self.bestLoss = 1e9
-        self.batchNum = 0
         if not os.path.exists(out_path):
             os.makedirs(out_path)
         self.out_path = out_path
@@ -77,18 +74,12 @@ class LearningRateFinder(object):
         K.set_value(self.model.optimizer.lr, lr)
 
     def run(self, train_generator, batchsize=1, samplesize=2048, epochs=8, steps_per_epoch=None):
-        # reset our class-specific variables
-
-        # if we're using a generator and the steps per epoch is not
-        # supplied, raise an error
         if steps_per_epoch is None:
             steps_per_epoch = np.ceil(len(train_generator) / float(batchsize))
 
         if epochs is None:
             epochs = int(np.ceil(samplesize / float(steps_per_epoch)))
-        # derive the learning rate multiplier based on the ending
-        # learning rate, starting learning rate, and total number of
-        # batch updates
+
         self.lrMult = (self.stop_lr / self.start_lr) ** (1.0 / (epochs*steps_per_epoch))
 
         # construct a callback that will be called at the end of each
@@ -104,18 +95,13 @@ class LearningRateFinder(object):
 
 def smooth_values(data_dict,metric='loss'):
     avgLoss = 0
-    bestLoss = 1e9
-    batchNum = 0
     beta = 0.98
     smooth_vals = []
     lrs = []
     for i in range(len(data_dict[metric])):
-        batchNum += 1
         loss = data_dict[metric][i]
         avgLoss = (beta * avgLoss) + ((1 - beta) * loss)
-        smooth = avgLoss / (1 - (beta ** batchNum))
-        if batchNum == 1 or smooth < bestLoss:
-            bestLoss = smooth
+        smooth = avgLoss / (1 - (beta ** i))
         smooth_vals.append(smooth)
         lrs.append(data_dict['learning_rate'][i])
     return lrs, smooth_vals
